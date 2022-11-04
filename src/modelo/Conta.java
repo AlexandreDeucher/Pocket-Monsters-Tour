7f77cb644ac5f1dbb0e52d6ce47ca8c2bf160be7
pfleger <pflegerdev@gmail.com>
2022-11-04 14:50:35 -0300

    update conta

--- src/modelo/Conta.java
+++ src/modelo/Conta.java
@@ -1,5 +1,29 @@
 package modelo;
 
-public class Conta {
+import java.time.LocalDate;
 
+public class Conta {
+	
+	private String name;
+	private String password;
+	private LocalDate dataNascimento;
+	private String email;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public LocalDate getDataNascimento(){
+		return dataNascimento;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	
 }
